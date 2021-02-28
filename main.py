1c4b0c56584847012167d713b79315a5b1a8d5b5
charmockridge <charmockridge@users.noreply.github.com>
2021-02-28 16:36:45 +0000

    updated code

--- main.py
+++ main.py
@@ -34,22 +34,17 @@ def Generate(passLen):
         list(string.punctuation)
     )
 
-    print(password)
-
     for i in range(0, passLen):
         indexChar = randint(0, len(characters) - 1)
         generation.append(characters[indexChar])
 
-    password = ''.join(str(x) for x in password)
-    print(generation)
-    print("=" + password)
     check = Secure(generation)
 
     # Error here
-    if check is True:
-        return password
-    elif check is False:
-        Generate(passLen)
+    if check is False:
+        return Generate(passLen)
+
+    return ''.join(str(x) for x in generation)
 
 
 def Main():
